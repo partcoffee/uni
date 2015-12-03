c53a30694ed1ba2fedad414be025e3e7d9b9ffc7
PartCoffee <partcoffee@users.noreply.github.com>
2015-12-03 22:05:01 +0100

    added more comments

--- js/main.js
+++ js/main.js
@@ -685,7 +685,7 @@ $(this).attr('id','item_'+index);
 	});
 });
 
-// Custom message for the curious who explore the console :)
+// Custom message for the curious who explore the console log :)
 console.log("Even after 400 generations in villages and cities, we haven't forgotten. The open road still softly calls, like a nearly forgotten song of childhood. We invest far-off places with a certain romance. This appeal, I suspect, has been meticulously crafted by natural selection as an essential element in our survival. Long summers, mild winters, rich harvests, plentiful game—none of them lasts forever.\n\nIt is beyond our powers to predict the future. Catastrophic events have a way of sneaking up on us, of catching us unaware. Your own life, or your band's, or even your species' might be owed to a restless few—drawn, by a craving they can hardly articulate or understand, to undiscovered lands and new worlds.\n\nHerman Melville, in Moby Dick, spoke for wanderers in all epochs and meridians – 'I am tormented with an everlasting itch for things remote. I love to sail forbidden seas...'\n\n– Carl Sagan");
 
 
